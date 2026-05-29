1d154805001bf7fd165c0d041407995a9a0ba2f1
Emily Stolf de Farias <emilystolfdefarias@gmail.com>
2026-05-29 19:23:52 -0300

    Add files via upload

---
+++ SistemaCaixaSupermercado.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args) {
+
+        Scanner leia = new 
+    Scanner(System.in);
+
+        double total=0;
+        double produto;
+        int resposta;
+
+        do{
+            System.out.println("Insira o valor do produto: ");
+            produto = leia.nextDouble();
+
+            total = total + produto;
+
+            System.out.println("TOTAL: " + total);
+
+            System.out.println("Aperte 1 para CONTINUAR ou 2 para FINALIZAR COMPRAS")
+            resposta= leia.nextint();
+
+            switch (resposta) {
+                case 1 
+                  // CONTINUAR
+                  break;
+
+                case 2:
+                System.out.println("TOTAL FINAL: " + total);
+                        break:
+                default:
+
+                System.out.println("Cancelar Compra"):
+                     }
+        } while (resposta !=2);
+
+        leia.close();
+        
+    }
+}
